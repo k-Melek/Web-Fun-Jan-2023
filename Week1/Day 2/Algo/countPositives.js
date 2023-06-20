a7211e5337cca1e0ce9ab8325f0f1e6e0e80522b
https://github.com/k-Melek <khedher.melek@gmail.com>
2023-06-20 10:26:54 +0100

    practice_2

---
+++ Week1/Day 2/Algo/countPositives.js
@@ -0,0 +1,8 @@
+var countPositives = 0;
+var numbers = [3, 4, -2, 7, 16, -8, 0]
+for(let i =0; i< numbers.length; i++){
+    if(numbers[i] > 0){
+    countPositives++;
+    }
+}
+console.log("there are " + countPositives +" positive values");
