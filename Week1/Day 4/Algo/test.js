37539eceb0fb44e14285aa5ed2aaf1a3957d264f
https://github.com/k-Melek <khedher.melek@gmail.com>
2023-06-22 15:28:33 +0100

    James_Consulting

---
+++ Week1/Day 4/Algo/test.js
@@ -0,0 +1,13 @@
+var x = 5;
+    
+function addToX(amount) {
+    return x + amount;
+    console.log("hello there");
+}
+    
+console.log(x);
+var result = addToX(-10);
+console.log(result);
+console.log(x);
+y=console.log(x)
+console.log(y)
